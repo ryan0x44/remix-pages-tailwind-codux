e6fb2f33c534853ff77cb2ea17c9e9cf054ec603
Ryan D <git@ryan0x44.com>
2023-07-24 15:07:18 +1000

    Added example component

---
+++ app/components/Example.tsx
@@ -0,0 +1,7 @@
+export default function() {
+    return (
+        <>
+            <div className="bg-red-500">Example Component</div>
+        </>
+    )
+}
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,7 @@
 import type { V2_MetaFunction } from "@remix-run/cloudflare";
 
+import ExampleComponent from "../components/Example";
+
 export const meta: V2_MetaFunction = () => {
   return [
     { title: "New Remix App" },
@@ -11,6 +13,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
+      <ExampleComponent />
       <ul>
         <li>
           <a
