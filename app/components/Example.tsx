f6a4b55bf40821bbe19c2f55fdab498eef3e16ef
Ryan D <git@ryan0x44.com>
2023-07-24 22:52:32 +1000

    Made use of example component clearer

--- app/components/Example.tsx
+++ app/components/Example.tsx
@@ -3,9 +3,11 @@ import { Button } from "flowbite-react";
 export default function() {
     return (
         <>
+            <div className="bg-red-500">Example Component Top</div>
+            <br />
             <Button>Flowbite Button</Button>
             <br />
-            <div className="bg-red-500">Example Component</div>
+            <div className="bg-red-500">Example Component Bottom</div>
         </>
     )
 }
--- codux/boards/new-board.board.tsx
+++ codux/boards/new-board.board.tsx
@@ -5,7 +5,6 @@ import Example from "../../app/components/Example";
 export default createBoard({
     name: 'New Board',
     Board: () => <div>
-        <div className="bg-red-500">Test</div>
         <Example />
     </div>
 });
